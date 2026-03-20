e51503fbe7bcd37ac4695b4987d70e65a0ffa8ba
alexanderjson <alexanderjson@hotmail.com>
2026-03-20 03:07:58 +0100

    centralised projects props, better version management, added targets and analyzer

---
+++ analyzers/MyApi.Analyzers/Class1.cs
@@ -0,0 +1,45 @@
+﻿namespace MyApi.Analyzers;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class Class1 : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "MY001";
+
+    private static readonly DiagnosticDescriptor Rule = new(
+        DiagnosticId,
+        "Banned phrase detected",
+        "Ta bort detta",
+        "Content",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+        context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.StringLiteralExpression);
+    }
+
+    private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+    {
+        var literal = (LiteralExpressionSyntax)context.Node;
+
+        var text = literal.Token.ValueText;
+
+        if (text.Contains("/*"))
+        {
+            var diagnostic = Diagnostic.Create(Rule, literal.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
